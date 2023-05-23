781efb799a03574eac412bb5f80a879ea2373820
Reshma Nidhish <reshmanidhish0@gmail.com>
2023-05-23 22:22:55 +0200

    iteration done styling not done

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Register the location for handlebars partials here:
 
 // ...
@@ -19,7 +19,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    beerImage: 'public/images/beer.png',
+    key: 'something'
+  });
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beer: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', function (req, res) {
+  punkAPI
+    .getRandom()
+    .then(function (responseFromAPI) {
+      const randomBeer = responseFromAPI[0];
+
+      res.render('random-beer', randomBeer );
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
